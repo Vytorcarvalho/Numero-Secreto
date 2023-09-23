1acf2913cd7d33534c569f4bf436ff99ee9bb3b9
Vytor Carvalho <127906923+Vytorcarvalho@users.noreply.github.com>
2023-09-23 19:05:22 -0300

    q

--- app/reconhecimentoDeFala.js
+++ app/reconhecimentoDeFala.js
@@ -1,7 +1,6 @@
 const elementoChute = document.getElementById('chute')
 
-const SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
-
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
@@ -9,17 +8,17 @@ recognition.start()
 
 recognition.addEventListener('result', onSpeak)
 
-function onSpeak (e){ 
-    chute= e.results[0][0].transcript
-    exibeChuteNaTela()
+function onSpeak(e) {
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
     verificaSeOChutePossuiUmValorValido(chute)
 }
 
-function exibeChuteNaTela (chute) {
+function exibeChuteNaTela(chute) {
     elementoChute.innerHTML = `
-        <div>Você disse:</div>
-        <span class="box">${chute} </span>
-    `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
 }
 
-recognition.addEventListener('end',()=>{recognition.start()})
+recognition.addEventListener('end', () => recognition.start())
