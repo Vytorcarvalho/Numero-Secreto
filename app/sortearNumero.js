a5c78c8bf0616390e7b825b97eea72522a72abf3
Vytor Carvalho <127906923+Vytorcarvalho@users.noreply.github.com>
2023-09-23 18:54:10 -0300

    app

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,15 +1,16 @@
-const menorValor = 1
-const maiorValor = 1000
+const menorValor = 20
+const maiorValor = 30
 const numeroSecreto = gerarNumeroAleatorio()
 
+
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * maiorValor + 1)
-}
+        return parseInt(Math.random() * (menorValor - maiorValor - 1)+(maiorValor + 1));
+    }
 
-console.log('Número Secreto:', numeroSecreto)
+console.log("O Número Secreto é: " + numeroSecreto) 
 
-const elementoMenorValor = document.getElementById('menor-valor')
+const elementoMenorValor = document.getElementById("menor-valor")
 elementoMenorValor.innerHTML = menorValor
 
-const elementoMaiorValor = document.getElementById('maior-valor')
+const elementoMaiorValor = document.getElementById("maior-valor")
 elementoMaiorValor.innerHTML = maiorValor
