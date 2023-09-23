5b6320d7e9f56afba60dc1b979d2d66ebdbae294
Vytor Carvalho <127906923+Vytorcarvalho@users.noreply.github.com>
2023-09-23 18:40:59 -0300

    test

--- app/reconhecimentoDeFala.js
+++ app/reconhecimentoDeFala.js
@@ -1,6 +1,6 @@
 const elementoChute = document.getElementById('chute')
 
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
@@ -8,17 +8,17 @@ recognition.start()
 
 recognition.addEventListener('result', onSpeak)
 
-function onSpeak (e){ 
-    chute= e.results[0][0].transcript
-    exibeChuteNaTela()
+function onSpeak(e) {
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
     verificaSeOChutePossuiUmValorValido(chute)
 }
 
-function exibeChuteNaTela (chute) {
+function exibeChuteNaTela(chute) {
     elementoChute.innerHTML = `
-        <div>Você disse:</div>
-        <span class="box">${chute} </span>
-    `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
 }
 
-recognition.addEventListener('end',()=>{recognition.start()})
+recognition.addEventListener('end', () => recognition.start())
--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,16 +1,15 @@
-const menorValor = 20
-const maiorValor = 30
+const menorValor = 1
+const maiorValor = 1000
 const numeroSecreto = gerarNumeroAleatorio()
 
-
 function gerarNumeroAleatorio() {
-        return parseInt(Math.random() *  (menorValor - maiorValor - 1) + (maiorValor + 1));
-    }
+    return parseInt(Math.random() * maiorValor + 1)
+}
 
-console.log("O Número Secreto é: "+numeroSecreto) 
+console.log('Número Secreto:', numeroSecreto)
 
-const elementoMenorValor = document.getElementById("menor-valor")
+const elementoMenorValor = document.getElementById('menor-valor')
 elementoMenorValor.innerHTML = menorValor
 
-const elementoMaiorValor = document.getElementById("maior-valor")
+const elementoMaiorValor = document.getElementById('maior-valor')
 elementoMaiorValor.innerHTML = maiorValor
